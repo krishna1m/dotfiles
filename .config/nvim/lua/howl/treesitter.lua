34d9841071c07519b0887d0abea049d592c522ee
Manmohan Krishna <krishna.m.zyw@gmail.com>
2023-10-03 12:46:10 +0530

    python treesitter disabled

--- .config/nvim/lua/howl/treesitter.lua
+++ .config/nvim/lua/howl/treesitter.lua
@@ -1,12 +1,12 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"elixir", "heex", "eex", "scala", "lua", "c", "rust", "toml", "python"}, 
+  ensure_installed = {"elixir", "heex", "eex", "scala", "lua", "c", "rust", "toml"}, 
   -- ensure_installed = "all", -- install parsers for all supported languages
   auto_install = true,
   ident = { enable = true },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
-    disable = { },
+    disable = {"python"},
   },
   rainbow = {
     enable = true,
