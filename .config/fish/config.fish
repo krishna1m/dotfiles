3cc9a758159c4bd2c05a1f6899f57b78f2d99a07
Manmohan Krishna <krishna.m.zyw@gmail.com>
2024-03-13 20:36:42 +0530

    haskell ghc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,6 +21,8 @@ set -x PATH $CARGO_ENV $CARGO_BIN $PATH
 # haskell
 # To start a simple repl, run:
 #   ghci
+# To create an executable for your haskell program, run:
+#   ghc hello.hs -o helloworld
 # To start a new haskell project in the current directory, run:
 #   cabal init --interactive
 # To install other GHC versions and tools, run:
